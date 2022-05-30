06485b476591d282e6ef7f468a8ccb02e047f276
Aryan Sharma <sharmaaryan0828@gmail.com>
2022-05-30 21:09:59 +0530

    Deleted backend folder

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require('mongoose')
-
-const monoURL='mongodb://localhost:27017/'
-
-const connectToMongo=(monoURL,()=>{
-    console.log('Connected to mongodb');
-})
-
-module.exports=connectToMongo;
