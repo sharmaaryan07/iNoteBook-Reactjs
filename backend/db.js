c6f106185583c6bde08b8f2238192aa9cb7548c8
Aryan Sharma <sharmaaryan0828@gmail.com>
2022-05-30 22:05:25 +0530

    connected backend with express and mongoose

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const mongoURI='mongodb://localhost:27017/?directConnection=true&tls=false&readPreference=primary'
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("connected")
+    })
+}
+
+module.exports=connectToMongo;
