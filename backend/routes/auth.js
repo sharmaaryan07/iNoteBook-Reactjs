31a50365b1d6163d20d1823747a7679e9bee64e4
Aryan Sharma <sharmaaryan0828@gmail.com>
2022-05-31 21:32:09 +0530

    Added collection in thunder client and improved the backend code

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 
-// Create a User using: POST "/api/auth/" or Doesn't require auth.
-router.post('/', [
+// Create a User using: POST "/api/auth/createuser" or Doesn't require auth.
+router.post('/createuser', [
     // name must be at least 3 chars long
     body('name', 'Enter a valid Name').isLength({ min: 3 }).withMessage('must be at least 5 chars long'),
     // username must be an email
@@ -13,20 +13,28 @@ router.post('/', [
     body('password').isLength({ min: 5 }).withMessage('must be at least 5 chars long').not().isIn(['123', 'password', 'god']).withMessage('Do not use a common word as the password').isLength({ min: 5 }).matches(/\d/),
 
 ], (req, res) => {
+    // If there is error show bad request and show errors.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user))
-        .catch(err => {
-            console.log(err)
+    // Check whthere user's email exist's already or not.
+    try {
+        // Create a new user.
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        }).then(user => res.json(user))
+            .catch(err => {
+                console.log(err)
 
-            res.json({ error: "Enter the unique Email" })
-        })
+                res.json({ error: "Enter the unique Email" })
+            })
+    } catch (error) {   // Catch if there is any error
+        console.error(error.message);
+        res.status(500).send("Some error occured")
+    }
 
 
 })
