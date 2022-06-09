aaaa756f1bda3da60301be12de16fca3b891ec98
Aryan Sharma <sharmaaryan0828@gmail.com>
2022-06-09 21:32:44 +0530

    Fetching all notes & Adding a Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,50 @@
-const express= require('express');
-const router= express.Router()
+const express = require('express');
+const router = express.Router()
+const fetchuser = require('../middleware/fetchuser');    // Imported  jsonwebtoken
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator');    //Imported express-validator
 
 
-router.get('/',(req,res)=>{
-    res.json([])
+
+//ROUTE 1: Get all notes using: GET "/api/notes/getuser". Logedin require.
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id })
+        res.json(notes)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error occure")
+    }
+})
+
+//ROUTE 2: Add a new note using: POST "/api/notes/addnote". Logedin require.
+router.post('/addnote', fetchuser, [
+    // title must be at least 3 chars long
+    body('title', 'ust be at least 3 chars long').isLength({ min: 3 }),
+    // desription must be at least 5 chars long
+    body('desription', 'must be at least 5 chars long').isLength({ min: 5 }),
+
+], async (req, res) => {
+    try {
+
+        const {title, desription, tag} = req.body;
+        // If there is error show bad request and show errors.
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const note = new Note({
+            title, desription, tag, user: req.user.id
+        })
+    
+        //It will save the note
+        const saveNote = await note.save()  
+        res.json(saveNote)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error occure")
+    }
 })
 
 module.exports = router;
