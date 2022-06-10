cdb52737754c21596a0d35b857772956799d026a
Aryan Sharma <sharmaaryan0828@gmail.com>
2022-06-10 16:25:42 +0530

    Added a folder 'component' in which I have added few component and also made Navbar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+
+import Navbar from './component/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import About from './component/About';
+import Home from './component/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello to myNoteBook</h1>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
