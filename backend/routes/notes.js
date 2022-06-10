b6297e7000a0e4927d787f19a26b5b186a14abf0
Aryan Sharma <sharmaaryan0828@gmail.com>
2022-06-10 14:55:48 +0530

    Made an EndPoint for Updating an existing note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -27,7 +27,7 @@ router.post('/addnote', fetchuser, [
 ], async (req, res) => {
     try {
 
-        const {title, desription, tag} = req.body;
+        const { title, desription, tag } = req.body;
         // If there is error show bad request and show errors.
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -37,14 +37,51 @@ router.post('/addnote', fetchuser, [
         const note = new Note({
             title, desription, tag, user: req.user.id
         })
-    
+
         //It will save the note
-        const saveNote = await note.save()  
+        const saveNote = await note.save()
         res.json(saveNote)
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal server error occure")
     }
+})
+
+
+//ROUTE 3: Update a note using: PUT "/api/notes/updatenote/:id". Logedin require.
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+    const { title, desription, tag } = req.body;
+    // Create newNote object for updating the notes
+    const newNote = {}
+
+    // If title then change it with newNote title
+    if (title) { 
+        newNote.title = title 
+    }
+    if (desription) { 
+        newNote.desription = desription 
+    }
+    if (tag) { 
+        newNote.tag = tag 
+    }
+
+
+    // Find the note to be updated and update it.
+    let note= await Note.findById(req.params.id);
+    if(!note){
+        return res.status(404).send("Not Found");
+    }
+
+    // To see if user is not changing note of other user's
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+
+    // To update the newNote
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true})
+    res.json({note})
 })
 
 module.exports = router;
