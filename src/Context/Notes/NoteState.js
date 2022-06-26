c045aa5f4c63dbe11d9a29a03119c25da1a7243d
Aryan Sharma <sharmaaryan0828@gmail.com>
2022-06-26 11:38:28 +0530

    Fetch Notes from Context.

--- src/Context/Notes/NoteState.js
+++ src/Context/Notes/NoteState.js
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import NoteContext from "./notesContext";
 
 const NoteState = (props) => {
 
+    const notesInitial=[
+        {
+          "_id": "62a217c3895b45e5af34d7c2",
+          "user": "62a1d08ce39def1bd1618e94",
+          "title": "Itachi of Hidden leaf",
+          "desription": "I will protect Hidden Leaf",
+          "tag": "Akatsuki",
+          "date": "2022-06-09T15:54:43.597Z",
+          "__v": 0
+        },
+        {
+          "_id": "62b7f462969eca708b36cdc6",
+          "user": "62a1d08ce39def1bd1618e94",
+          "title": "My Title",
+          "desription": "Naruto",
+          "tag": "Anime",
+          "date": "2022-06-26T05:53:38.699Z",
+          "__v": 0
+        }
+      ]
+
+      const [notes,setNote]=useState(notesInitial);
    
 
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes,setNote}}>
             {props.children}
         </NoteContext.Provider>
     )
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Home.css'
+import notesContext from '../Context/Notes/notesContext'
 
 function Home() {
+
+    const context = useContext(notesContext);
+
+    const { notes, setNote } = context;
+
     return (
         <div className='container mt-4'>
             <h2 className='d-flex justify-content-center'>Add a Note</h2>
 
             <form className=''>
                 <div className="form-floating mb-3 ">
-                    <input type="email" className="form-control " id="floatingInput" placeholder="name@example.com"/>
-                        <label htmlFor="floatingInput" className='start'>Email address</label>
+                    <input type="email" className="form-control " id="floatingInput" placeholder="name@example.com" />
+                    <label htmlFor="floatingInput" className='start'>Email address</label>
                 </div>
                 <div className="form-floating">
-                    <input type="current-password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                        <label htmlFor="floatingPassword" className='start2'>Password</label>
+                    <input type="current-password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <label htmlFor="floatingPassword" className='start2'>Password</label>
                 </div>
-        
+
                 <button type="submit" className="mt-3 btn btn-primary">Submit</button>
             </form>
 
-            <h2 className='mt-5 text-center'>Your Note</h2>
+            <div>
+                <h2 className='mt-5 text-center'>Your Note</h2>
+
+                {notes.map((note)=>{
+                    return note.desription;
+                })}
+            </div>
         </div>
     )
 }
